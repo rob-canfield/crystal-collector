c7db701ea8460e106a8f3594e9f63f5cfe72542d
Rob Canfield <git@livesenjoyable.com>
2019-08-04 21:09:21 -0600

    trying new addition function from kind people on stack overflow. Still not working. Added win/loss variables and DOM.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,84 @@
 // Click button ---> Random number generates to begin game.
 
 
-var randomNum = Math.floor(Math.random() * 120) + 19;
-    console.log(randomNum);
-    $("#rndm-nmbr").append("<h2>" + randomNum + "</h2>");
-
-
+function randomNum() 
+    {randomNum = Math.floor(Math.random() * 120) + 19;
+        console.log(randomNum);
+        $("#rndm-nmbr").append("<h2>" + randomNum + "</h2>");
+    }
+randomNum();
 
 var redCrystalNum = Math.floor(Math.random() * 13) + 1;
 var blueCrystalNum = Math.floor(Math.random() * 13) + 1;
 var yellowCrystalNum = Math.floor(Math.random() * 13) + 1;
 var greenCrystalNum = Math.floor(Math.random() * 13) + 1;
 
+
+function crystalNums() {
     $("#redcrystal").on('click', function () {   
          
         if (redCrystalNum !== blueCrystalNum && redCrystalNum !== yellowCrystalNum && redCrystalNum !== greenCrystalNum){
-            scoreSum.push(redCrystalNum); 
+            crystals.push(redCrystalNum); 
+            sum = arrsum(crystals)
         } else {
             redCrystalNum = Math.floor(Math.random() * 13) + 1;
         } 
     });
 
-
-
-
     $("#bluecrystal").on('click', function () { 
         if (blueCrystalNum !== redCrystalNum && blueCrystalNum !== yellowCrystalNum && blueCrystalNum !== greenCrystalNum){
-            scoreSum.push(blueCrystalNum); 
+            crystals.push(blueCrystalNum); 
         } else {
             blueCrystalNum = Math.floor(Math.random() * 13) + 1;
         }   
     });
 
-
-
-
-
     $("#yellowcrystal").on('click', function () {
         if (yellowCrystalNum !== redCrystalNum && yellowCrystalNum !== blueCrystalNum && yellowCrystalNum !== greenCrystalNum){
-            scoreSum.push(yellowCrystalNum); 
+            crystals.push(yellowCrystalNum); 
         } else {
             yellowCrystalNum = Math.floor(Math.random() * 13) + 1;
         } 
     });
 
-
-
     $("#greencrystal").on('click', function () {  
         if (greenCrystalNum !== redCrystalNum && greenCrystalNum !== blueCrystalNum && greenCrystalNum !== yellowCrystalNum){
-            scoreSum.push(greenCrystalNum);
+            crystals.push(greenCrystalNum);
         } else {
             greenCrystalNum = Math.floor(Math.random() * 13) + 1;
         }   
     });
+}
+crystalNums();
 
 var crystals = []
+console.log(crystals);
+
 var sum = 0
-if (crystals.length > 0){
-    for (i = 0; i < crystals.length; i++)
+
+function arrsum(arr) {
+    sum = (arr.reduce(function(xs, x) {
+        xs = xs + x
+    }, 0))
+}
+
+var totalScore = sum
+
+var wins = 0
+var losses = 0
+
+if (totalScore === randomNum){
+    wins++
+    $("#wins").append("WINS: " + wins);
+    randomNum();
+    crystalNums();
+    alert("You're a freaking wizard");
+} else if (totalScore > randomNum){
+    losses++
+    $("#losses").append("LOSSES: " + losses);
+    randomNum();
+    crystalNums();
+    alert("try again...")
 }
 
 
